c1608a89ee5a3c48a84d3cd6f6559416c0f16a3e
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-12-05 19:48:51 +0530

    sets

---
+++ Frozenset.py
@@ -0,0 +1,9 @@
+d_c={"Thinnava","Em chesthunav","Yekada unnav","Class ipoindha"}
+print(d_c)
+print(type(d_c))
+print(d_c)
+print(d_c)
+print(d_c)
+x = frozenset(range(1,10))
+print(x)
+print(type(x))
---
+++ Sets.py
@@ -0,0 +1,6 @@
+d_c={"Thinnava","Em chesthunav","Yekada unnav","Class ipoindha"}
+print(d_c)
+print(type(d_c))
+print(d_c)
+print(d_c)
+print(d_c)
